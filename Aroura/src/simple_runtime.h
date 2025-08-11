bebee5d8cb51d0e4c55e1e01f7d52b2fca4ded8e
William Jones <129543744+buttcakesss@users.noreply.github.com>
2025-08-11 16:21:58 -0400

    Delete Aroura directory

--- Aroura/src/simple_runtime.h
+++
@@ -1,9 +0,0 @@
-#ifndef SIMPLE_RUNTIME_H
-#define SIMPLE_RUNTIME_H
-
-// Function prototypes
-int run_wasm_with_runtime(const char* wasm_file);
-int run_wasm_with_wat_wrapper(const char* wasm_file);
-int run_wasm_file(const char* wasm_file);
-
-#endif // SIMPLE_RUNTIME_H
